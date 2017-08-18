f8ab409dce8c4484ae5dfc913900f8f2fab87b78
James Preiss <japreiss@usc.edu>
2017-08-18 16:59:57 -0700

    play song on all 6 props

--- src/modules/interface/tilthex_song.h
+++ src/modules/interface/tilthex_song.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 // plays a short song using the propeller speeds to generate tones.
-
+// uses all 6 motors, so useful as a hardware power-on self-test.
 void songBegin(int I2CAddr);
 
 void songStep(int tick);
--- src/modules/src/tilthex_song.c
+++ src/modules/src/tilthex_song.c
@@ -11,48 +11,47 @@
 #define G 392
 #define Ah 440
 #define r 0
-#define x 1
 
 // `r` is a rest - the propeller will stop.
 // `h` is a hold - no motor command will be sent.
 // note that e.g. {D, h} and {D, D} are equivalent.
 
 static int songCursor;
+static int motorCursor;
 static int noteStartTick;
 static int noteTicks;
 static bool songDone;
 static int I2CAddr;
+
+void songBegin(int addr)
+{
+	songCursor = 0;
+	motorCursor = 0;
+	noteStartTick = -10000;
+	noteTicks = 0;
+	songDone = false;
+	I2CAddr = addr;
+}
 
 // it's important to have a rest (r) at the end,
 // otherwise the last tone will go on forever.
 
 // "Redbone" by Childish Gambino
 static int const song[] = {
-	x, x, x, x, x, x, x, x, D, D, D, r, Ah, Ah, r, G, r, F, r, E, r, C, r, A, r, D, D, D, D, r, };
+	r, r, r, r, r, r, r, r, D, D, D, r, Ah, Ah, r, G, r, F, r, E, r, C, r, A, r, D, D, D, D, r, };
 
-static void setTone(int hz)
+// only one motor is ever playing a tone... to avoid taking off
+static void setTone(int motor, int hz)
 {
-	if (hz > 1) {
-		// tones are one octave too high for 6-inch props :)
-		hz = hz / 2;
-	}
+	// at 385hz, 1500/4096 pulsewidth means motor stopped but ESC still armed
+	int const d0 = 1500;
 
-	uint16_t const dur = 
-		(hz == r) ? 0 :
-		(5 * hz) / 2 + 1471;
+	uint16_t durations[6] = { d0, d0, d0, d0, d0, d0 };
 
-	if (hz != x) {
-		pca9685setDurationsAsync(I2CAddr, 0, 1, &dur);
-	}
-}
-
-void songBegin(int addr)
-{
-	songCursor = 0;
-	noteStartTick = -10000;
-	noteTicks = 0;
-	songDone = false;
-	I2CAddr = addr;
+	// tones are one octave too high for 6-inch props :)
+	hz = hz / 2;
+	durations[motor] = (5 * hz) / 2 + 1471;
+	pca9685setDurationsAsync(I2CAddr, 0, 6, durations);
 }
 
 // assumes that tick always >= 0.
@@ -63,8 +62,18 @@ void songStep(int tick)
 		songDone = true;
 	}
 	else if (tick - noteStartTick >= noteTicks) {
-		// tones are an octave too high for 6" props...
-		setTone(song[songCursor]);
+		int const note = song[songCursor];
+		int const prevNote = songCursor > 0 ? song[songCursor - 1] : r;
+
+		if (note == r) {
+			// stop all motors
+			setTone(0, 0);
+		}
+		else if (note != prevNote) {
+			setTone(motorCursor, note);
+			motorCursor = (motorCursor + 1) % 6;
+		}
+
 		++songCursor;
 		noteStartTick = tick;
 		noteTicks = 100;
