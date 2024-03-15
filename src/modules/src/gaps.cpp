60365196f8d54f278ed3bd140198b90aea9f8ce3
jpreiss <jamesalanpreiss@gmail.com>
2024-03-14 18:39:38 -0700

    GAPS WIP

--- src/modules/src/gaps.cpp
+++ src/modules/src/gaps.cpp
@@ -17,6 +17,23 @@ Eigen::Matrix<float, dimU, dimT> dudtheta;
 Eigen::Matrix<float, 1, dimX> dcdx;
 Eigen::Matrix<float, 1, dimU> dcdu;
 
+void gaps_init(float dt)
+{
+    auto I = Eigen::Matrix<float, 3, 3>::Identity();
+    dxdx.setIdentity();
+    dxdx.block<3, 3>(3, 3) = dt * I;
+    dxdu.setZero();
+    dxdu.block<3, 3>(3, 0) = dt * I;
+    // TODO: dudtheta
+}
+
+void gaps_fill(float pos_err[3], float vel_err[3], float p_cost, float v_cost, float u_cost)
+{
+    dcdx.block<1, 3>(0, 0) = p_cost * pos_err;
+    dcdx.block<1, 3>(0, 3) = v_cost * vel_err;
+    // TODO: u and dcdu
+}
+
 // updates the global variables using the inputs
 void gaps_update(float eta)
 {
