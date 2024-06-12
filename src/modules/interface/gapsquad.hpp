bd228e748605afab15c3ba5a42e9f2172088aa46
jpreiss <jamesalanpreiss@gmail.com>
2024-06-11 19:56:21 -0700

    derivs 6f469 was wrong

--- src/modules/interface/gapsquad.hpp
+++ src/modules/interface/gapsquad.hpp
@@ -330,7 +330,7 @@ extern "C" bool gaps_step(
 
 	// 1) compute the gradient
 	Eigen::Map<GapsY> y(gaps->y[0]);
-	Theta grad = (Dc_x + (Dc_u * Du_x) * y + Dc_u * Du_t).array();
+	Theta grad = ((Dc_x + Dc_u * Du_x) * y + Dc_u * Du_t).array();
 	// regularization. TODO: account for gradmode?
 	grad += gaps->cost_param.reg_L2 * theta;
 
